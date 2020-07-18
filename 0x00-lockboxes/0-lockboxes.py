b49b46cf688120969cc080bec35bc10490c3bafa
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-18 16:54:17 +0000

    First commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def getKeys(i, keys, boxes):
+    """ Try to open boxes depending on the keys """
+    if (i < len(boxes)):
+        keys = getKeys(i + 1, updateKeys(keys, boxes, i), boxes)
+    else:
+        i = len(boxes) - 1
+    return keys
+
+def updateKeys(oldKeys, boxes, i):
+    """ Open boxes with current keys """
+    newKeys = set(oldKeys)
+    for i in oldKeys:
+        try:
+            boxes[i]
+        except:
+            pass
+        else:
+            for b in boxes[i]:
+                newKeys.add(b)
+    return newKeys
+
+def canUnlockAll(boxes):
+    """ Unlocks all boxes """
+    myKeys = set(boxes[0])
+    h = getKeys(0, myKeys, boxes)
+    for i in range(1, len(boxes)):
+        if i not in h:
+            return False
+    return True
