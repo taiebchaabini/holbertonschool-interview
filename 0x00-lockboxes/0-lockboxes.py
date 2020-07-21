7802a60f7b7573e870f5c6186a2b0360131c7b1f
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-21 23:18:38 +0000

    Update 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -19,7 +19,7 @@ def canUnlockAll(boxes):
                 visited[element] = True
             for node in boxes[element]:
                 if not visited[node]:
-                    stack.append(node) 
+                    stack.append(node)
         except:
             pass
     return all(visited)
