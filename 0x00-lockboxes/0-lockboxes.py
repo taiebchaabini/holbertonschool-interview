bd07e2cbd2411a88530634c81ef99d6d285ff703
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-07-21 23:11:51 +0000

    New changes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,9 +3,13 @@
 
 def canUnlockAll(boxes):
     """ Unlocks all boxes """
+    if (len(boxes) == 1):
+        return True
+    if (len(boxes) <= 0):
+        return False
     visited = [False for v in range(len(boxes))]
-    notVisited  = [[] for s in range(len(boxes))]
-    stack = [boxes]
+    visited[0] = True
+    stack = boxes[0]
 
     while(len(stack)):
         element = stack[-1]
@@ -13,9 +17,9 @@ def canUnlockAll(boxes):
         try:
             if not visited[element]:
                 visited[element] = True
-            for node in notVisited[element]:
+            for node in boxes[element]:
                 if not visited[node]:
                     stack.append(node) 
         except:
             pass
-    return len(visited) == len(boxes)
+    return all(visited)
