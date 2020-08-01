8f4bfc3546c4663beca54ca3f6c064fe97018dca
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-01 19:53:07 +0100

    Betty fix

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
     new->n = value;
     new->left = NULL;
     new->right = NULL;
-    
+
     if (parent != NULL)
     {
         if (parent->n > value)
