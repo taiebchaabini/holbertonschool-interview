6ce2ff3b8ed4e55aa2692b6c158c042a6bd24445
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-01 20:37:33 +0100

    Update 0-binary_tree_node.c

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,23 +5,24 @@
  * @value: is the value to put in the new node
  * Return: returns a pointer to the new node, or NULL on failure
 **/
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
-    binary_tree_t *new;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+        binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return NULL;
-    new->parent = parent;
-    new->n = value;
-    new->left = NULL;
-    new->right = NULL;
+        new = malloc(sizeof(binary_tree_t));
+        if (new == NULL)
+                return (NULL);
+        new->parent = parent;
+        new->n = value;
+        new->left = NULL;
+        new->right = NULL;
 
-    if (parent != NULL)
-    {
-        if (parent->n > value)
-            parent->left = new;
-        else
-            parent->right = new;
-    }
-    return new;
+        if (parent != NULL)
+        {
+                if (parent->n > value)
+                        parent->left = new;
+                else
+                        parent->right = new;
+        }
+        return (new);
 }
