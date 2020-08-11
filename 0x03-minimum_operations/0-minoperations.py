eb693303ea8e7fb1ebbb7d50075dd7901c8228f2
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-08-11 15:44:33 +0100

    First commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Calculates the fewest number of operations needed to result in exactly n H characters. """
+def minOperations(n):
+    copy = "H"
+    output = ""
+    i = 0
+    if (n == 1):
+        return 1
+    while (len(output) < n):
+        if (i % 2 == 0):
+            output += copy
+        else:
+            copy = output
+            output += copy
+        i += 1
+    if (i == n):
+        return i
+    return i + 1
