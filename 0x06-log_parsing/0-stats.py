b89194368086c5f9809f40f0b9eed23c8f23b047
Taieb Chaabini <taiebchaabini@gmail.com>
2020-08-31 19:19:08 +0100

    First commit

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import collections
+""" Script that reads stdin line by line and computes metrics. """
+i = 0
+size = 0
+key = ""
+metrics = collections.OrderedDict({})
+for line in sys.stdin:
+    b = [str(x) for x in line.split(' ') if x.strip()]
+    key = b[-2]
+    try:
+        metrics[key] += 1
+    except:
+        metrics[key] = 0
+    size += int(b[-1])
+    i += 1
+    if (i == 10):
+        print("File size: " + str(size))
+        for k in sorted(metrics):
+            print(k + ": " + str(metrics[k]))
+        i = 0
