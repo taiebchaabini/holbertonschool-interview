1e7d0cf943426f132e0330fe82b58eed25e38a22
Taieb Chaabini <taiebchaabini@gmail.com>
2020-11-04 23:57:04 +0100

    Adding condition

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -66,6 +66,9 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	avl_t *head = NULL;
 
+	if (array == NULL || size <= 0)
+		return (NULL);
+
 	head = insert_start_end(NULL, array, 0, size - 1);
 	if (head == NULL)
 	{
