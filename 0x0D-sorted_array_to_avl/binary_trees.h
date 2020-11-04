7b378166644918a27a305165f2dce630de229460
Taieb Chaabini <taiebchaabini@gmail.com>
2020-11-04 23:32:37 +0100

    trying with 2 fn

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -26,7 +26,7 @@ typedef struct binary_tree_s avl_t;
 avl_t *insert_into_avl(avl_t *head, int value);
 avl_t *insert_start_end(avl_t *start, int *array, int first, int last);
 avl_t *sorted_array_to_avl(int *array, size_t size);
-void binary_tree_print(const binary_tree_t *);
+
 
 #endif
 
