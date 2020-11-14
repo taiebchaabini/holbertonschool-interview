43a77821d717cf3f663682f19e4fa85b19d02904
Taieb Chaabini <taiebchaabini@gmail.com>
2020-11-14 19:48:59 +0100

    Fixing semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,34 +1,35 @@
 #!/usr/bin/node
-console.log()
-let movieID = process.argv[2];
+const movieID = process.argv[2];
 const request = require('request');
 
-
-request({url: 'https://swapi-api.hbtn.io/api/films/' + movieID, json: true}, function (error, response, body) {
-  if (response && response.statusCode == 200){
-    var res = {}
+request({ url: 'https://swapi-api.hbtn.io/api/films/' + movieID, json: true }, function (error, response, body) {
+  if (error) {
+    return;
+  }
+  if (response && response.statusCode === 200) {
+    const res = {};
     // console.log('body:', body); // Print the HTML for the Google homepage.
- 
+
     body.characters.forEach(element => {
-        let id = element.replace("https://swapi-api.hbtn.io/api/people", "");
-        id = id.split("/")[1];
-        request({url: element, json: true}, function(error, response, body){
-            if (response && response.statusCode == 200){
-                //console.log(body.name);
-                res[id] = body.name;
-            }
-        });
+      let id = element.replace('https://swapi-api.hbtn.io/api/people', '');
+      id = id.split('/')[1];
+      request({ url: element, json: true }, function (error, response, body) {
+        if (error) {
+          return;
+        }
+        if (response && response.statusCode === 200) {
+          // console.log(body.name);
+          res[id] = body.name;
+        }
+      });
+    });
 
-    });;
-
-    setTimeout(function(){
-        body.characters.forEach(elem => {
-            let id = elem.replace("https://swapi-api.hbtn.io/api/people", "");
-            id = id.split("/")[1];
-            console.log(res[id]);
-        });
-    }, 3000)
-   
-  } 
+    setTimeout(function () {
+      body.characters.forEach(elem => {
+        let id = elem.replace('https://swapi-api.hbtn.io/api/people', '');
+        id = id.split('/')[1];
+        console.log(res[id]);
+      });
+    }, 3000);
+  }
 });
-
