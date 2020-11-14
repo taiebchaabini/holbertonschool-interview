f7f8ba512850f6951968c6abf5b402d0c8cdbc21
Taieb Chaabini <taiebchaabini@gmail.com>
2020-11-14 19:42:24 +0100

    Fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,11 +3,10 @@ console.log()
 let movieID = process.argv[2];
 const request = require('request');
 
-var res = {}
 
 request({url: 'https://swapi-api.hbtn.io/api/films/' + movieID, json: true}, function (error, response, body) {
-
   if (response && response.statusCode == 200){
+    var res = {}
     // console.log('body:', body); // Print the HTML for the Google homepage.
  
     body.characters.forEach(element => {
@@ -15,13 +14,21 @@ request({url: 'https://swapi-api.hbtn.io/api/films/' + movieID, json: true}, fun
         id = id.split("/")[1];
         request({url: element, json: true}, function(error, response, body){
             if (response && response.statusCode == 200){
-                console.log(body.name);
+                //console.log(body.name);
+                res[id] = body.name;
             }
         });
 
     });;
 
- 
+    setTimeout(function(){
+        body.characters.forEach(elem => {
+            let id = elem.replace("https://swapi-api.hbtn.io/api/people", "");
+            id = id.split("/")[1];
+            console.log(res[id]);
+        });
+    }, 3000)
+   
   } 
 });
 
