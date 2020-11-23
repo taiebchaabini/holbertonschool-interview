7da4078d0cdd39f2dfac96c5ca4e39020ed5e82f
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-11-23 23:58:34 +0100

    New changes

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 """
 Calculate how much water will be retained after it rains.
 Wall width is 1
@@ -12,6 +13,10 @@ def rain(walls):
         return (0)
     for element in walls:
         i += 1
-        if (i != len(walls)):
-            rainAmount += element
+        if (i < len(walls) - 1 and i % 2 == 0):
+            if (abs(walls[i - 1] - walls[i + 1]) > walls[i]):
+                if (walls[i - 1] < walls[i + 1]):
+                    rainAmount += walls[i - 1]
+                elif(walls[i - 1] > walls[i + 1]):
+                    rainAmount += walls[i + 1]
     return (rainAmount)
