a521daa09a9520ad842fedb776aa4e6355837ff4
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-11-24 09:22:01 +0100

    New method

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,14 +8,14 @@ Wall width is 1
 def rain(walls):
     i = 0
     rainAmount = 0
+    rainFill = 0
     if (len(walls) == 0):
         return (0)
     for element in walls:
         i += 1
         if (i > 0 and i < len(walls) - 1):
-            if (abs(walls[i - 1] - walls[i + 1]) > walls[i]):
-                if (walls[i - 1] < walls[i + 1]):
-                    rainAmount += walls[i - 1]
-                elif(walls[i - 1] > walls[i + 1]):
-                    rainAmount += walls[i + 1]
+            if (walls[i - 1] > walls[i]):
+                rainFill = abs(walls[i - 1] - walls[i])
+                rainAmount += rainFill
+                walls[i] += rainFill
     return (rainAmount)
