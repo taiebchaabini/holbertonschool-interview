21b9a8c603f4326fd59e05224c59d73da26b1090
Taieb Chaâbini <taiebchaabini@gmail.com>
2020-12-23 15:03:01 +0000

    Adding condition for null arrays

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -62,5 +62,7 @@ int binary_search(int *array, size_t start, size_t end, int value)
  **/
 int advanced_binary(int *array, size_t size, int value)
 {
+	if (array == NULL)
+		return (-1);
 	return (binary_search(array, 0, size - 1, value));
 }
