f2e7f2de5078ac5930e20922fa3c047dedcaede0
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-01-27 22:53:10 +0000

    Fixing requirement

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -11,27 +11,28 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *slow;
 	listint_t *fast;
 
-    if (!head || !head->next)
-        return (NULL);
+	if (!head || !head->next)
+		return (NULL);
 
-    slow = head->next;
-    fast = head->next->next;
+	slow = head->next;
+	fast = head->next->next;
 
 	while (fast && fast->next && slow != fast)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
 
-    if (slow != fast)
-        return (NULL);
+	if (slow != fast)
+		return (NULL);
 
-    slow = head;
+	slow = head;
 
-    while (slow != fast){
-        slow = slow->next;
-        fast = fast->next;
-    }
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
 
 	return (slow);
 }
