98b2f78d923d189af333ae43ef79e51b05224511
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-02-27 19:36:14 +0000

    Fc

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+"""
+def makeChange(coins, total):
+    if (total <= 0):
+        return (-1)
+    coins = reversed(sorted(coins))
+    opSum = 0
+    for i in coins:
+        opSum += int(total / i)
+        total -= int(total / i) * i
+        if (total == 0):
+            return (opSum)
+    return (-1)
