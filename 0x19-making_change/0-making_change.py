eed1b9a5cb182f04ecfda0ac02fda59e23b889c5
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-02-27 21:01:55 +0000

    Adding comment

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,6 +6,10 @@ fewest number of coins needed to meet a given amount total.
 
 
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the
+    fewest number of coins needed to meet a given amount total.
+    """
     if (total <= 0):
         return (-1)
     coins = reversed(sorted(coins))
