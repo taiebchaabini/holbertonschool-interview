f73bebea6ecc0434fd2730415a6727fbfc2c1872
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-02-27 19:44:45 +0000

    Fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -10,12 +10,9 @@ def makeChange(coins, total):
         return (-1)
     coins = reversed(sorted(coins))
     opSum = 0
-    hit = total
     for i in coins:
         opSum += int(total / i)
         total -= int(total / i) * i
         if (hit == 0):
             return (opSum)
-        if (hit < 0):
-            return (-1)
     return (-1)
