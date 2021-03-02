5e22e7866248965b7a11c1e2e5d17b3a2b9b575f
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-03-02 11:04:20 +0000

    Adding prototype

--- 0x1A-double_circular_linked_list/list.h
+++ 0x1A-double_circular_linked_list/list.h
@@ -18,6 +18,7 @@ typedef struct List
 	struct List *next;
 } List;
 
+List *create_node(char *str);
 List *add_node_end(List **list, char *str);
 List *add_node_begin(List **list, char *str);
 #endif
