de304ee1918e9180c5585b3be6003c5ab87956da
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-03-15 14:52:52 +0000

    First commit

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Finds the perimeter of the island """
+
+
+def island_perimeter(grid):
+    """ Finds the perimeter of the island """
+    cSum = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if (grid[y][x] != 0):
+                cSum += 4
+                if (x + 1 < len(grid[y]) - 1 and grid[y][x + 1] != 0):
+                    cSum -= 1
+                if (x - 1 > 0 and grid[y][x - 1] != 0):
+                    cSum -= 1
+                if (y - 1 > 0 and grid[y - 1][x] != 0):
+                    cSum -= 1
+                if (y + 1 < len(grid) - 1 and grid[y + 1][x] != 0):
+                    cSum -= 1
+    return cSum
