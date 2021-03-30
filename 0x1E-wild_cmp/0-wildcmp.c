d7f3c394bf77bdfad9564269a1eb4ee19137e062
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-03-30 19:37:02 +0000

    FC

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * wildcmp - Function that compares two string
+ * @s1: String 1 to be compared
+ * @s2: String 2 to be compared - May contain the wildcard "*"
+ * Return: returns 1 if the strings can be considered identical,
+ * otherwise return 0.
+ **/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (s2[0] == '*')
+	{
+		if (s2[1] == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (s1[0] != s2[1])
+			return (wildcmp(s1 + 1, s2));
+		else
+			return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
