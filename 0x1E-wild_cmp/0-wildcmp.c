55596475c6fac017dd6f295180fc7f4dd1a9fcaa
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-03-30 19:48:54 +0000

    Fixing pattern wildcmp(main-main.c, ma*in.c)

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -21,8 +21,8 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (s2[1] == '*')
 			return (wildcmp(s1, s2 + 1));
-		if (s1[0] != s2[1])
-			return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1 + 1, s2))
+			return (1);
 		else
 			return (wildcmp(s1, s2 + 1));
 	}
