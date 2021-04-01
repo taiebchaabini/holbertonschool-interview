71667e84726c538895abaa127c612f906a748a79
Taieb Chaâbini <taiebchaabini@gmail.com>
2021-04-01 12:15:34 +0000

    FC

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Returns a list of lists of integers representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    myList = [[1]]
+    if (n <= 0):
+        return [[]]
+    for i in range(1, n):
+        myList.append([1])
+        for b in range(1, i):
+            tmp = int(myList[i - 1][b]) + int(myList[i - 1][b - 1])
+            myList[i].append(str(tmp))
+        myList[i].append(1)
+    return myList
